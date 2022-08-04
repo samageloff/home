3cf07df80b84bcccce5568ecc8cb2ff8b20d0512
Sam Ageloff <samageloff@stockx.com>
2022-08-04 16:36:12 +0000

    adding default export

--- pages/theme/index.js
+++ pages/theme/index.js
@@ -1,6 +1,6 @@
 import { extendTheme } from "@chakra-ui/react"
 
-export const theme = extendTheme({
+const theme = extendTheme({
   styles: {
     global: {
       'html, body': {
@@ -12,3 +12,4 @@ export const theme = extendTheme({
   },
 })
 
+export default theme
